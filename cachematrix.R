3977df34c63c1a83620219d94de6554322ac7beb
Jenslorens <jenslorens@gmail.com>
2015-07-27 00:38:24 +0200

    My reply
    This one confused me as I never saw the x$function…. code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  
+  # Initiate the local storage to NULL
+  
+  CacheMatrix <- NULL
+  
+  # Assign value to main argumnet
+  setMatrix <- function(Incoming) {
+    x <<- Incoming
+    CacheMatrix <<- NULL
+  }
+  
+  # Get the value from the argument
+  getMatrix <- function() {
+    x
+  }
+ 
+  # cache the inversed argument
+  cacheInverse <- function(solve) {
+    CacheMatrix <<- solve
+  }
+  
+  # get the cached value
+  getInverse <- function() {
+    CacheMatrix
+  }
+  
+  list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function (y, ...) {
+  # Obtain the cached value
+  Result <- y$getInverse()
+  # Check if the value exists, then return it
+  if(!is.null(Result)) {
+    message("data was cached")
+    return(Result)
+  }
+  # If first time, calculate the inverse and store in cache
+  else {
+    data <- y$getMatrix()
+    Result <- solve(data)
+    y$cacheInverse(Result)
+  }
+  
+  Result
+  
 }
+
